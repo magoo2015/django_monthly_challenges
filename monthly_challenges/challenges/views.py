cb2d8248e74737a52dce8849e191a1d1eaabf0c4
Sydney McGee <mcgee.sydney@gmail.com>
2024-06-25 22:26:13 -0500

    Added dynamic path segments for january - march

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,7 +1,18 @@
+from curses.ascii import HT
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse("This works!")
+def monthly_challenge(request, month):
+    challenge_text = None
+    if month  == "january":
+        challenge_text = "Do strenthg training"
+    elif month == "february":
+        challenge_text = "Walk for atleast 45 min everyday!"
+    elif month == "march":
+        challenge_text = "Learn Django for at least 20 min everyday!"
+    else:
+        return HttpResponseNotFound("This month is not supported")
+    return HttpResponse(challenge_text)
+
