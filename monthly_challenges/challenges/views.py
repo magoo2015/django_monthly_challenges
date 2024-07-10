6182bf7483296e0aa96dd3a9688645f5b23d5911
Sydney McGee <mcgee.sydney@gmail.com>
2024-07-09 23:29:48 -0500

    Added templates folder w/challenge.html file, registered challenge app in settings.py

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
 
+
 monthly_challenges = {
     "january": "Eat no meat",
     "february": "Walk for at least 20 min",
@@ -47,7 +48,7 @@ def monthly_challenge_by_number(request, month):
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        response_data = f"<h1>{challenge_text}</h1>"
-        return HttpResponse(response_data)
+        return render(request, "challenges/challenge.html")
+        
     except:
         return HttpResponseNotFound(f"<h1>This month is not supported</h1>")
